2188ae5d28329bd45c6fafeaad868ae83bb1453b
christianrieck <chrissrieck@googlemail.com>
2026-02-14 08:02:25 +0000

    Deploying to gh-pages from @ christianrieck/christianrieck.github.io@6290f716e5da7c9d0a7ce899631e0eebdcb658c6 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -369,8 +369,8 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather.html";
-            },},{id: "news-our-dagstuhl-seminar-proposal-on-discrete-reconfiguration-of-geometric-graphs-drawings-and-arrangements-has-been-accepted",
-          title: 'Our Dagstuhl seminar proposal on ‘‘Discrete Reconfiguration of Geometric Graphs, Drawings, and Arrangements’’...',
+            },},{id: "news-our-dagstuhl-seminar-discrete-reconfiguration-of-geometric-graphs-drawings-and-arrangements-has-been-accepted",
+          title: 'Our Dagstuhl seminar ‘‘Discrete Reconfiguration of Geometric Graphs, Drawings, and Arrangements’’ has been...',
           description: "",
           section: "News",},{id: "news-our-papers-disproving-two-conjectures-on-the-hamiltonicity-of-venn-diagrams-and-tilt-automata-gathering-particles-with-uniform-external-control-have-been-accepted-for-publication-at-socg-2026",
           title: 'Our papers Disproving two conjectures on the Hamiltonicity of Venn diagrams and Tilt...',
